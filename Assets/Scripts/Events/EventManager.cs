b4c3b05fc320f5a4f0da0f3a221404013fb1284f
Egehan EMRE <16008122049@ogr.bozok.edu.tr>
2025-01-19 05:56:07 +0300

    Update EventManager.cs

--- Assets/Scripts/Events/EventManager.cs
+++ Assets/Scripts/Events/EventManager.cs
@@ -1,49 +1,69 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
     [Header("Event Cache")]
-    [SerializeField] private GameObject[] events = new GameObject[] {};
-    private List<IEvent> eventSlots = new List<IEvent>();
-    private List<EventWeight> calculatedEventWeights = new List<EventWeight>();
-    [SerializeField] private float totalWeight = 0;
-    
+    [SerializeField] private GameObject[] events = new GameObject[] { };
+    private readonly List<IEvent> eventSlots = new List<IEvent>();
+    private readonly List<EventWeight> calculatedEventWeights = new List<EventWeight>();
+    private float totalWeight = 0;
+
     [Header("Event Throwing")]
     [SerializeField] private float throwInterval = 5f;
     [SerializeField] private float intervalRandomization = 2f;
     [SerializeField] private float throwRate = 0.85f;
     private float throwTimer = 0;
 
-    private void FixedUpdate() {
-        throwTimer -= Time.fixedDeltaTime;
-        if(throwTimer <= 0) ThrowingTick();
+    private void Start()
+    {
+        CalculateWeights();
     }
 
-    public void ThrowingTick() {
-        throwTimer = UnityEngine.Random.Range(throwInterval, throwInterval*intervalRandomization);
-        
-        if (UnityEngine.Random.value < throwRate) {
+    private void FixedUpdate()
+    {
+        throwTimer -= Time.fixedDeltaTime;
+        if (throwTimer <= 0)
+        {
+            PerformThrowingTick();
+        }
+    }
+
+    private void PerformThrowingTick()
+    {
+        throwTimer = UnityEngine.Random.Range(throwInterval, throwInterval * intervalRandomization);
+
+        if (UnityEngine.Random.value < throwRate)
+        {
             ThrowRandomEvent();
         }
     }
 
-    private void Start() { CalculateWeights(); }
     public void CacheEvents(GameObject[] objects)
     {
         eventSlots.Clear();
-        foreach (var eventPiece in objects) {
-            eventSlots.Add(eventPiece.GetComponent<IEvent>());
+        foreach (var eventPiece in objects)
+        {
+            var eventComponent = eventPiece.GetComponent<IEvent>();
+
+            if (eventComponent != null)
+            {
+                eventSlots.Add(eventComponent);
+            }
+            else
+            {
+                Debug.LogWarning($"GameObject {eventPiece.name} does not implement IEvent");
+            }
         }
     }
 
-    public void ThrowRandomEvent() {
+    public void ThrowRandomEvent()
+    {
         Shuffle(calculatedEventWeights);
         float randomWeight = UnityEngine.Random.Range(0, totalWeight);
         float currentWeight = 0;
-        
+
         foreach (var eventPiece in calculatedEventWeights)
         {
             currentWeight += eventPiece.Weight;
@@ -57,22 +77,37 @@ public class EventManager : MonoBehaviour
         }
     }
 
-    public void CalculateWeights() {
+    public void CalculateWeights()
+    {
+        if (events == null || events.Length == 0)
+        {
+            Debug.LogWarning("No events configured in the inspector");
+            return;
+        }
+
         CacheEvents(events);
         calculatedEventWeights.Clear();
         totalWeight = 0;
-        
-        foreach (var eventSlot in eventSlots) {
+
+        foreach (var eventSlot in eventSlots)
+        {
             float weight = eventSlot.GetWeight();
-            calculatedEventWeights.Add(new EventWeight {Weight = weight, EventRef = eventSlot});
+            calculatedEventWeights.Add(new EventWeight { Weight = weight, EventRef = eventSlot });
             totalWeight += weight;
         }
+
+        if (totalWeight <= 0)
+        {
+            Debug.LogError("Total weight of all events is zero or negative");
+        }
     }
-    
-    public void Shuffle(List<EventWeight> ts) {
+
+    public void Shuffle(List<EventWeight> ts)
+    {
         var count = ts.Count;
         var last = count - 1;
-        for (var i = 0; i < last; ++i) {
+        for (var i = 0; i < last; ++i)
+        {
             var r = UnityEngine.Random.Range(i, count);
             var tmp = ts[i];
             ts[i] = ts[r];
