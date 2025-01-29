cd886237a446e1590a9f308cf80a3f9402cd849e
YasinAhmet <77017655+YasinAhmet@users.noreply.github.com>
2025-01-29 12:22:00 +0300

    Fixed extinguisher

--- Assets/Scripts/Props/Extinguisher.cs
+++ Assets/Scripts/Props/Extinguisher.cs
@@ -9,11 +9,8 @@ public class Extinguisher : MonoBehaviour
 {
       [SerializeField] private LayerMask extinguishableLayer; 
       [SerializeField] private float extinguishRange = 5f;
-      [SerializeField] private float extinguishDelay = 0.1f;
       [SerializeField] private float extinguishSpeed = 100;
-      [SerializeField] private float spread = 0.3f;
-      [SerializeField] private Transform startLocation;
-      private float extinguishTime = 0;
+      [SerializeField] private Transform collisionCheckZone;
       private ObjectsGrabbable grabbableRef;
       private bool extinguishing = false;
       [SerializeField] private ParticleSystem extinguishingEffect;
@@ -23,9 +20,6 @@ public class Extinguisher : MonoBehaviour
       }
       
       private void Update() {
-            extinguishTime -= Time.deltaTime;
-            if(extinguishTime > 0) return;
-
             bool previousExtinguishingState = extinguishing;
             extinguishing = Input.GetKey(KeyCode.E) && grabbableRef.IsGrabbed();
             if (previousExtinguishingState != extinguishing) {
@@ -36,20 +30,16 @@ public class Extinguisher : MonoBehaviour
                   }
                   else extinguishingEffect.Stop();
             }
+      }
 
+      private void FixedUpdate()
+      {
             if (extinguishing) {
-                  extinguishTime = extinguishDelay;
-                  Vector3 rightSpread = transform.right * spread;
-                  Vector3 upSpread = transform.up * spread;
-                  if (Random.Range(0f, 1f) > 0.5f) rightSpread *= -1;
-                  if (Random.Range(0f, 1f) > 0.5f) upSpread *= -1;
-                  Vector3 spreadPosition = rightSpread + upSpread;
-                        
-                  bool didHit = Physics.Raycast(startLocation.position, transform.forward + spreadPosition, out RaycastHit hitInfo,
-                        extinguishRange, extinguishableLayer);
-                  //Debug.DrawRay(startLocation.position, transform.forward+spreadPosition, Color.blue, 0.25f);
-                  if (didHit && hitInfo.collider.gameObject.TryGetComponent<Fire>(out Fire fire)) {
-                        fire.Extinguish(extinguishSpeed*Time.deltaTime);
+                  Collider[] overlaps = Physics.OverlapBox(collisionCheckZone.position, collisionCheckZone.lossyScale/2, Quaternion.identity, extinguishableLayer);
+                  foreach (var overlap in overlaps) {
+                        if (overlap.gameObject.TryGetComponent<Fire>(out Fire fire)) {
+                              fire.Extinguish(extinguishSpeed*Time.fixedDeltaTime);
+                        }   
                   }
             }
       }
