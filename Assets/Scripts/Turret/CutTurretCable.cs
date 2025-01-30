1fdf7aea732ff5c05ae1103566b545155b646b99
YasinAhmet <77017655+YasinAhmet@users.noreply.github.com>
2025-01-30 20:26:03 +0300

    Material and visual overhaul

--- Assets/Scripts/Turret/CutTurretCable.cs
+++ Assets/Scripts/Turret/CutTurretCable.cs
@@ -7,6 +7,11 @@ public class CutTurretCable : MonoBehaviour, IinteractionInformationTaker
 {
     [SerializeField] private AudioSource fixingSound;
     [SerializeField] private Turret linkedTurret;
+    [SerializeField] private float playCutSoundDelay;
+    [SerializeField] private AudioClip cutSound;
+    [SerializeField] private float volume = 0.03f;
+    private float timePassed = 0;
+    
     public void Fix() {
         linkedTurret.RecoverEnergy();
         gameObject.SetActive(false);
@@ -15,5 +20,15 @@ public class CutTurretCable : MonoBehaviour, IinteractionInformationTaker
     
     public void HasBeenHoldingSince(float time, bool holdingNow) {
         fixingSound.mute = !holdingNow;
+    }
+
+    private void Update() {
+        if (gameObject.activeSelf) {
+            timePassed += Time.deltaTime;
+            if (timePassed > playCutSoundDelay) {
+                AudioSource.PlayClipAtPoint(cutSound, transform.position, volume);
+                timePassed = 0;
+            }
+        }
     }
 }
