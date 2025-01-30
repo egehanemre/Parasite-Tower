d74893f3e6b605ad8cba6d28a10ce81f25762025
YasinAhmet <77017655+YasinAhmet@users.noreply.github.com>
2025-01-30 21:39:59 +0300

    Sacrifice point field added

--- Assets/Scripts/Body/SacrificePoint.cs
+++ Assets/Scripts/Body/SacrificePoint.cs
@@ -7,9 +7,10 @@ public class SacrificePoint : MonoBehaviour
     [SerializeField] private UpgradeManager upgradeManager;
     [SerializeField] private List<ParticleSystem> bloodEffects = new List<ParticleSystem>();
     [SerializeField] private AudioClip audio;
+    [SerializeField] private int moneyGain = 1;
 
     public void Sacrifice(GameObject skullObject) {
-        upgradeManager.Money++;
+        upgradeManager.Money += moneyGain;
         upgradeManager.UpdateMoneyText();
         Destroy(skullObject);
         foreach (var bloodEffect in bloodEffects) {
